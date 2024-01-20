65b0a99497c8f4ceb52d24ef1b22c4a11c0ec467
c0destaker <codestaker@protonmail.com>
2024-01-20 21:23:32 +1100

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model({'User', userSchema})
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
