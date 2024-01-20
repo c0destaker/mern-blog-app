b1940459a8d7f500ab61ef53a257a422936522c7
c0destaker <codestaker@protonmail.com>
2024-01-20 21:01:35 +1100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -13,3 +15,5 @@ mongoose
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
+
+app.use("/api/user", userRoutes);
